868827bd61532a48cf6b82d093420dcb2daa5736
Jesus Espinoza Villar <141283532+22-JesusEspinoza@users.noreply.github.com>
2023-09-12 09:51:30 -0500

    clases

---
+++ ClasesEjercicio/src/clasesejercicio/ClasesEjercicio.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package clasesejercicio;
+
+/**
+ *
+ * @author Lab18
+ */
+public class ClasesEjercicio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        persona objPersona1 = new persona(); 
+        persona objPersona2 = new persona();
+       
+       
+       objPersona1.comer();
+       objPersona2.dormir();
+       objPersona1.correr();
+       objPersona2.año();
+       objPersona1.dormir();
+    }
+    
+}
---
+++ ClasesEjercicio/src/clasesejercicio/persona.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clasesejercicio;
+
+/**
+ *
+ * @author Lab18
+ */
+public class persona {
+    
+    
+//Atributos        
+    String nombre;
+    int edad; 
+    
+    
+//metodos (acciones)  
+public void comer(){ 
+  System.out.println("comi");
+            }
+public void dormir(){ 
+  System.out.println("me dormi");
+            }
+
+
+//sobrecarga de metodos        
+public void correr(){
+}
+public void correr(int a){
+}
+
+
+//metodo con valor de retorno    
+public int año(){
+
+}
+public String texto(){
+return "hola soy jesus y esta es mi clase";
+}
+public float num() {
+    return 12.2f;
+}
+
+//metodo con parametros y argumentos        
+public void metodo1 (int a){ 
+    
+}
+public void metodo2(String a){
+}
+}
+
+
+
