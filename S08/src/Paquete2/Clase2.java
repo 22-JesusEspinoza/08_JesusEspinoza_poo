d6d4727e777af2d1d924ec4a203f8ebfa26fd887
Jesus Espinoza Villar <141283532+22-JesusEspinoza@users.noreply.github.com>
2023-09-15 09:13:22 -0500

    Actividad

---
+++ S08/src/Paquete2/Clase2.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Paquete2;
+
+/**
+ *
+ * @author Zec
+ */
+public class Clase2 {
+    
+    private String b;
+    private int d;
+}
---
+++ S08/src/jesus_poo/Jesus_poo.java
@@ -0,0 +1,21 @@
+package jesus_poo;
+
+import Paquete1.Clase1;
+import Paquete2.Clase2;
+
+public class Jesus_poo {
+
+    
+    public static void main(String[] args) {
+        
+        Clase1 C1 = new Clase1();
+        Clase2 C2 = new Clase2();
+        
+        C1.b = 20;
+        C1.c = "Mi numero de orden es el 8";
+        C2.a = "Soy jesus espinoza Villar y este es mi trabajo";
+        C2.d = 80;
+        
+    }
+    
+}
